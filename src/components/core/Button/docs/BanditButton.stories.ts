016aa116cec57298344c888857e2b9d90450fdef
Andrei Buiciuc <abuiciuc33@gmail.com>
2023-11-14 21:18:22 +0200

    improve components folder structure

--- src/components/core/Button/docs/BanditButton.stories.ts
+++ src/components/core/Button/docs/BanditButton.stories.ts
@@ -1,153 +1,154 @@
-import type { Meta, StoryObj } from "@storybook/vue3";
-import { MailCheck } from "lucide-vue-next";
-import BanditButton from "@/components/core/Button/BanditButton.vue";
+import type { Meta, StoryObj } from '@storybook/vue3'
+import { MailCheck } from 'lucide-vue-next'
+import BanditButton from '@/components/core/Button/BanditButton.vue'
 
-type Story = StoryObj<typeof BanditButton>;
+type Story = StoryObj<typeof BanditButton>
 
-const meta: Meta<typeof BanditButton> = { 
+const meta: Meta<typeof BanditButton> = {
   component: BanditButton,
   argTypes: {
     label: {
-      control: "text",
-      description: "Button label"
+      control: 'text',
+      description: 'Button label'
     },
     variant: {
-      control: "select",
-      description: "Button variant, controls the style of the button",
-      options: ["default", "secondary", "destructive", "outline", "link"]
+      control: 'select',
+      description: 'Button variant, controls the style of the button',
+      options: ['default', 'secondary', 'destructive', 'outline', 'link']
     },
     size: {
-      control: "select",
-      description: "Button sizes, controls the height of the button",
-      options: ["default", "mini", "compact", "large"]
+      control: 'select',
+      description: 'Button sizes, controls the height of the button',
+      options: ['default', 'mini', 'compact', 'large']
     },
     disabled: {
-      control: "boolean",
-      description: "Controls whether the button is disabled"
+      control: 'boolean',
+      description: 'Controls whether the button is disabled'
     },
     rounded: {
-      control: "boolean",
-      description: "Controls whether the button is rounded"
-    },
-  },
-};
+      control: 'boolean',
+      description: 'Controls whether the button is rounded'
+    }
+  }
+}
 
 /**
  * Used for important, non-destructive user actions
  */
 export const Primary: Story = {
   args: {
-    label: "Primary"
+    label: 'Primary'
   },
   render: (args) => ({
     components: { BanditButton },
     setup: () => {
-      return { args };
+      return { args }
     },
     template: '<BanditButton v-bind="args" />'
   })
-};
+}
 
 /**
  * Used in conjuction with the primary button to perform the complementary flow
  */
 export const Secondary: Story = {
   args: {
-    label: "Secondary",
-    variant: "secondary",
+    label: 'Secondary',
+    variant: 'secondary'
   },
   render: (args) => ({
     components: { BanditButton },
     setup: () => {
-      return { args };
+      return { args }
     },
     template: '<BanditButton v-bind="args" />'
   })
-};
+}
 
 /**
  * Used for actions with a destructive effect
  */
 export const Destructive: Story = {
   args: {
-    label: "Destructive",
-    variant: "destructive"
+    label: 'Destructive',
+    variant: 'destructive'
   },
   render: (args) => ({
     setup: () => {
-      return { args };
+      return { args }
     },
     components: { BanditButton },
     template: '<BanditButton v-bind="args" />'
   })
-};
+}
 
 /**
  * Used for secondary or tertiary actions and may denote a related flow
  */
 export const Outline: Story = {
   args: {
-    label: "Outline",
-    variant: "outline",
+    label: 'Outline',
+    variant: 'outline'
   },
   render: (args) => ({
-      components: { BanditButton },
-      setup: () => {
-        return { args };
-      },
-      template: '<BanditButton v-bind="args" />'
+    components: { BanditButton },
+    setup: () => {
+      return { args }
+    },
+    template: '<BanditButton v-bind="args" />'
   })
-};
+}
 
 /**
  * Used for secondary or tertiary actions and may denote a less likely action
  */
 export const Ghost: Story = {
   args: {
-    label: "Ghost",
-    variant: "ghost"
+    label: 'Ghost',
+    variant: 'ghost'
   },
   render: (args) => ({
     components: { BanditButton },
     setup: () => {
-      return { args };
+      return { args }
     },
     template: '<BanditButton v-bind="args" />'
-  }),
-};
+  })
+}
 
 /**
  * Used for actions that require a redirect
  */
 export const Link: Story = {
   args: {
-    label: "Link",
-    variant: "link"
+    label: 'Link',
+    variant: 'link'
   },
   render: (args) => ({
     components: { BanditButton },
     setup: () => {
-      return { args };
+      return { args }
     },
     template: '<BanditButton v-bind="args" />'
   })
-};
+}
 
 /**
  * Buttons accept the usage of icons before or after the label, using the corresponding slots.
  */
 export const WithIcon: Story = {
   args: {
-    label: "Check your Email",
-    variant: "default",
+    label: 'Check your Email',
+    variant: 'default'
   },
   render: (args) => ({
     components: { BanditButton, MailCheck },
     setup: () => {
-      return { args };
+      return { args }
     },
-    template: '<BanditButton v-bind="args"><template #append-icon><MailCheck /></template></BanditButton>'
+    template:
+      '<BanditButton v-bind="args"><template #append-icon><MailCheck /></template></BanditButton>'
   })
-};
+}
 
-export default meta;
+export default meta
---
+++ src/components/core/index.ts
@@ -0,0 +1,3 @@
+import BanditButton from '@/components/core/Button/index'
+
+export default { BanditButton }
